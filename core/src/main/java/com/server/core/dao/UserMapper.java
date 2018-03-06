d2ffd4b9ecda241b0f1d5767cb249f0bb88d5756
wangyujie <979731285@qq.com>
2018-03-06 13:30:28 +0800

    '添加了mybatis的支持'

---
+++ core/src/main/java/com/server/core/dao/UserMapper.java
@@ -0,0 +1,22 @@
+package com.server.core.dao;
+
+import com.server.core.pojo.User;
+import com.server.core.pojo.UserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserMapper {
+    long countByExample(UserExample example);
+
+    int deleteByExample(UserExample example);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    List<User> selectByExample(UserExample example);
+
+    int updateByExampleSelective(@Param("record") User record, @Param("example") UserExample example);
+
+    int updateByExample(@Param("record") User record, @Param("example") UserExample example);
+}
