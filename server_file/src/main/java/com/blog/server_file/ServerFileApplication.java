1f123b4adb50e7c6de94e7f90354f8a250d194b2
wangyujie <979731285@qq.com>
2018-03-06 10:10:17 +0800

    first submit

---
+++ server_file/src/main/java/com/blog/server_file/ServerFileApplication.java
@@ -0,0 +1,12 @@
+package com.blog.server_file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerFileApplication.class, args);
+	}
+}
